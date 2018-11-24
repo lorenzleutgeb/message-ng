68de67a2928371746c3a5947476d3e586124f5c6
Billie Rosalie Postlmayr <a1307120@unet.univue.ac.at>
2018-11-24 16:08:11 +0100

    enum and circumplex

---
+++ app/src/main/java/com/hackjunction/messageng/CircumplexModel.java
@@ -0,0 +1,32 @@
+package com.hackjunction.messageng;
+
+import java.util.EnumMap;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class CircumplexModel implements EmotionalStateInterface{
+    ArousalValenceDirectMapping mapping;
+    EmotionCategories category;
+    public CircumplexModel() {
+        this.mapping = new ArousalValenceDirectMapping();
+    }
+
+    public EmotionCategories getEmotion(double[] inputSet){
+        double x = mapping.getValence(inputSet);
+        double y = mapping.getArousal(inputSet);
+        double r = Math.sqrt(x*x + y*y);
+        double angle = Math.atan2(y, x);
+        double flippedAngle = Math.PI / 2 - angle;
+        if ((y>0 && x<0)||(y<0 && x<0)) angle = angle +  Math.PI;
+        if (x>0 && y<0) angle = angle +  Math.PI*2;
+        int theta = (int) angle;
+        return category.valueOf(theta);
+    }
+
+    @Override
+    public boolean getCurrentEmotionalState() {
+
+        return false;
+    }
+}
