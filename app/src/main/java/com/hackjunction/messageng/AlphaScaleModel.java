7d6ebf3cd02faf0d65446e621f717e4d9969958c
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2018-11-24 22:13:45 +0100

    Rewrite AlphaScaleModel for Java 7

--- app/src/main/java/com/hackjunction/messageng/AlphaScaleModel.java
+++ app/src/main/java/com/hackjunction/messageng/AlphaScaleModel.java
@@ -4,13 +4,18 @@ import java.util.List;
 import java.util.Map;
 
 public class AlphaScaleModel implements EmotionalStateInterface{
-
     @Override
     public boolean getCurrentEmotionalState(Map<BrainWave, List<Double>> measurements) {
-        List<Double> a = measurements.get(BrainWave.ALPHA);
-        double avg = a.stream()
-                .mapToDouble(x -> x)
-                .average().getAsDouble();
-        return avg >0.5?true:false;
+        double sum = 0;
+        double cnt = 0;
+        for (Double it : measurements.get(BrainWave.ALPHA)) {
+            if (it == null || Double.isNaN(it)) {
+                continue;
+            }
+            sum += it;
+            cnt += 1;
+        }
+
+        return (sum / cnt) > 0.5;
     }
 }
