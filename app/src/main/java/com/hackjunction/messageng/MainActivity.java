b2bff2a46a6095164240a01110144b78d2739b36
brian-regan <brianmjregan@gmail.com>
2018-11-25 02:10:04 +0200

    UI full port

--- app/src/main/java/com/hackjunction/messageng/MainActivity.java
+++ app/src/main/java/com/hackjunction/messageng/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
+import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -14,6 +16,10 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.firebase.firestore.DocumentReference;
@@ -22,11 +28,16 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -67,10 +78,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // Brian Code ¡¡¡¡¡
+    private LineGraphSeries<DataPoint> series;
+
+    private final Handler handler= new Handler();
+
+    private final Runnable r = new Runnable() {
+        private int x = 0;
+        @Override
+        public void run() {
+            series.appendData(
+                    new DataPoint(x,getRandom()),false, 100, false
+            );
+            x++;
+            handler.postDelayed(r,20);
+        }
+    };
+
+    double start = -5;
+    double end = 5;
+    Random mRand = new Random();
+
+    private double getRandom() {
+        return start + (mRand.nextDouble() * (end - start));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    // ¡¡¡
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // !! Brian Code
+
+        handler.post(r);
+
+        getSupportActionBar().hide();
+        initGraph();
+        // ¡¡
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             permissionCheck();
@@ -101,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     Toast.makeText(MainActivity.this, o.toString(), Toast.LENGTH_LONG).show();
 
-                    updateUI(o.toString());
+                    //updateUI(o.toString());
 
                 } else {
                     Log.d(TAG, "Current data: null");
@@ -115,10 +160,47 @@ public class MainActivity extends AppCompatActivity {
         localBroadcastManager.registerReceiver(broadcastReceiver, filter);
 
         startService(new Intent(MainActivity.this, MuseService.class));
+
+
+
     }
 
-    public void updateUI(String stateString) {
+    /*public void updateUI(String stateString) {
         TextView tv1 = findViewById(R.id.stateText);
         tv1.setText(stateString);
+    }*/
+
+    void initGraph() {
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        this.series = new LineGraphSeries<>();
+        series.setThickness(15);
+        series.setColor(Color.argb(50, 255, 255, 255));
+        series.setBackgroundColor(Color.argb(50, 255, 255, 255));
+        series.setDrawBackground(true);
+        graph.addSeries(series);
+        graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.NONE);// It will remove the background grids
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);// remove horizontal x labels and line
+        graph.getGridLabelRenderer().setVerticalLabelsVisible(false);
+    }
+
+
+    public void didTapPlayButton(View view) {
+        animateButton();
+    }
+
+    void animateButton() {
+        // Load the animation
+        final Animation myAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+
+        // Use custom animation interpolator to achieve the bounce effect
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+
+        myAnim.setInterpolator(interpolator);
+
+        // Animate the button
+        Button button = (Button)findViewById(R.id.button);
+        button.startAnimation(myAnim);
+
+
     }
 }
